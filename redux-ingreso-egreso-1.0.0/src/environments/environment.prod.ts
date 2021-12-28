29b50e62be1124c516163574af63cf42080974f1
MiguDeveloper <miguel.peru.seo@gmail.com>
2021-12-27 21:10:09 -0500

    primer commit

---
+++ redux-ingreso-egreso-1.0.0/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyABxFLpPASA8D2Hg1fh-AeWgC2KPZLkTbE",
+    authDomain: "ingreso-egreso-app-ffeee.firebaseapp.com",
+    projectId: "ingreso-egreso-app-ffeee",
+    storageBucket: "ingreso-egreso-app-ffeee.appspot.com",
+    messagingSenderId: "908876784424",
+    appId: "1:908876784424:web:ea8b41b86f554af873261d",
+  },
+};
---
+++ redux-ingreso-egreso-1.0.0/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyABxFLpPASA8D2Hg1fh-AeWgC2KPZLkTbE",
+    authDomain: "ingreso-egreso-app-ffeee.firebaseapp.com",
+    projectId: "ingreso-egreso-app-ffeee",
+    storageBucket: "ingreso-egreso-app-ffeee.appspot.com",
+    messagingSenderId: "908876784424",
+    appId: "1:908876784424:web:ea8b41b86f554af873261d",
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
